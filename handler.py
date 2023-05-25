eef1bb6ace5ced47d9b7167d53554569663bd10a
KwanjiChoi <kwanji0921@gmail.com>
2023-05-26 02:12:24 +0900

    Add docker definition for non-pure-python packages

--- handler.py
+++ handler.py
@@ -1,3 +1,8 @@
+try:
+  import unzip_requirements
+except ImportError:
+  pass
+
 import logging
 import os
 from slack_bolt import App
@@ -5,15 +10,16 @@ from slack_bolt.adapter.aws_lambda import SlackRequestHandler
 import openai
 import time
 import boto3
-from llama_index import GPTSimpleVectorIndex, download_loader
+from llama_index import GPTVectorStoreIndex, download_loader
 
-openai.api_key = os.getenv('OPENAI_API_KEY')
+openai.api_key = os.environ['OPENAI_API_KEY']
 
 app = App(
     process_before_response=True,
     token=os.environ["SLACK_BOT_TOKEN"],
     signing_secret=os.environ["SLACK_SIGNING_SECRET"]
 )
+
 
 def acknowledge(ack):
     # Acknowledge a request within 3 seconds, to prevent Slack from retrying the request.
